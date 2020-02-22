394a5f04809685242600f8f460d91ec9c68eae1f
Admin <vladimirleonov789@gmail.com>
2020-02-22 19:51:21 +0300

    cicles

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+let questions = ['как ваша фалилия?','как ваше имя?','как ваше отчество?'];
 let answers = [];
 
-answers[0] = prompt('как ваша фалилия?');
-answers[1] = prompt('как ваше имя?');
-answers[2] = prompt('как ваше отчество?');
+for (let i = 0; i < questions.length; i++) {
+    answers[i] = prompt(questions[i], '');
+}
 
 console.log(answers); 
+
+// let i = 0;
+// do {
+//     answers[i] = prompt (questions[i], '');
+//     i++;
+// } while(i < questions.length)
+// console.log (answers);
+
+// let i = 0;
+// while (i < questions.length) {
+//     answers[i] = prompt(questions[i], '');
+//     i++;
+// }
+// console.log(answers);
